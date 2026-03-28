2471f8afccf380460902bf44c431b0289395f932
Anch <adrianpratt.ap@gmail.com>
2026-03-28 20:44:56 +1300

    Added nil check to fix error

--- AtlasLoot/Databases/DatabaseFunctions.lua
+++ AtlasLoot/Databases/DatabaseFunctions.lua
@@ -95,7 +95,7 @@ function AtlasLoot:GetSourceList()
 	local function addItem(itemData, dataType)
 		if type(itemData) == "table" then
 			local typeData = self:GetDataType(dataType)
-			if typeData and itemData.itemID then
+			if typeData and self.Difficulties[typeData] and itemData.itemID then
 				for _, dif in ipairs(self.Difficulties[typeData]) do
 					local itemType = GetItemInfoInstant(itemData.itemID) or nil
 					if dif[2] ~= 3 and itemType then
